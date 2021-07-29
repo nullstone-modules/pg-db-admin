d348abecfd48de1530c902edaa310d210ab5406b
Brad Sickles <brad.sickles@gmail.com>
2021-07-29 12:29:33 -0400

    Added example tf

---
+++ aws/example/lambda.tf
@@ -0,0 +1,18 @@
+resource "aws_lambda_function" "db_admin" {
+  function_name = var.name
+  tags          = var.tags
+  role          = aws_iam_role.db_admin.arn
+  package_type  = "Image"
+  image_uri     = var.image_uri
+
+  environment {
+    variables = {
+      DB_CONN_URL_SECRET_ID = aws_secretsmanager_secret.db_admin_pg.id
+    }
+  }
+
+  vpc_config {
+    security_group_ids = var.network.security_group_ids
+    subnet_ids         = var.network.subnet_ids
+  }
+}
---
+++ aws/example/variables.tf
@@ -0,0 +1,48 @@
+variable "image_uri" {
+  description = "The docker image to utilize. By default, this uses nullstone's 'latest' publicly available image."
+  type        = string
+  default     = "public.ecr.aws/"
+}
+
+variable "name" {
+  description = "The name of the lambda function and role"
+  type        = string
+}
+
+variable "tags" {
+  description = "A map of tags that are applied to AWS resources"
+  type        = map(string)
+}
+
+variable "host" {
+  description = "The database cluster endpoint to connect"
+  type        = string
+}
+
+variable "database" {
+  description = "The initial database to connect. By default, uses 'postgres'"
+  type        = string
+  default     = "postgres"
+}
+
+variable "username" {
+  description = "Postgres username"
+  type        = string
+}
+
+variable "password" {
+  description = "Postgres password"
+  type        = string
+}
+
+variable "network" {
+  description = "Network configuration"
+  type = object({
+    security_group_ids : list(string)
+    subnet_ids = list(string)
+  })
+  default = {
+    security_group_ids = []
+    subnet_ids         = []
+  }
+}
