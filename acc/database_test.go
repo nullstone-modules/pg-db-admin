ca1d5415b3b7db97269d9f99c3517abb50fbf430
Brad Sickles <brad.sickles@gmail.com>
2021-08-04 10:21:45 -0400

    fix tests

--- acc/database_test.go
+++ acc/database_test.go
@@ -3,6 +3,7 @@ package acc
 import (
 	_ "github.com/lib/pq"
 	"github.com/nullstone-modules/pg-db-admin/postgresql"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"os"
 	"testing"
@@ -16,15 +17,18 @@ func TestDatabase(t *testing.T) {
 	db := createDb(t)
 	defer db.Close()
 
+	database := postgresql.Database{Name: "test-database"}
+
+	ownerRole := postgresql.Role{Name: database.Name}
+	require.NoError(t, ownerRole.Ensure(db), "error creating owner role")
+	database.Owner = ownerRole.Name
+
 	dbInfo, err := postgresql.CalcDbConnectionInfo(db)
 	require.NoError(t, err, "calc db info")
 
-	database := postgresql.Database{
-		Name:  "test-database",
-		Owner: "test-user",
-	}
 	require.NoError(t, database.Create(db, *dbInfo), "unexpected error")
 
 	find := &postgresql.Database{Name: "test-database"}
 	require.NoError(t, find.Read(db), "read database")
+	assert.Equal(t, ownerRole.Name, find.Owner, "mismatched owner")
 }
