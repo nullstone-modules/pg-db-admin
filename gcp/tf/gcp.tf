285877f576d7b3d82abd7344b06a1b49f06c19ba
Brad Sickles <brad.sickles@gmail.com>
2024-03-20 13:20:55 -0400

    Added cloudbuild API permission

--- gcp/tf/gcp.tf
+++ gcp/tf/gcp.tf
@@ -5,3 +5,25 @@ locals {
   region        = data.google_client_config.this.region
   region_prefix = lower(substr(local.region, 0, 2))
 }
+
+resource "google_project_service" "run" {
+  service                    = "run.googleapis.com"
+  disable_dependent_services = false
+  disable_on_destroy         = false
+}
+resource "google_project_service" "cloudbuild" {
+  service                    = "cloudbuild.googleapis.com"
+  disable_dependent_services = false
+  disable_on_destroy         = false
+}
+resource "google_project_service" "function" {
+  service                    = "cloudfunctions.googleapis.com"
+  disable_dependent_services = false
+  disable_on_destroy         = false
+}
+// Artifact Registry is needed to create a Cloud Function (db-admin) even though we're not using it
+resource "google_project_service" "artifact_registry" {
+  service                    = "artifactregistry.googleapis.com"
+  disable_dependent_services = false
+  disable_on_destroy         = false
+}
